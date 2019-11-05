cfd4b615494169236a484e4726972f72f9a5b57b
Core Ireland <irelandco@icloud.com>
2019-11-05 11:12:16 -0500

    New autos

---
+++ TeamCode/src/main/java/Competition/Programs/Autonomous/NewBlueFoundation.java
@@ -0,0 +1,60 @@
+package Competition.Programs.Autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import Competition.Robot;
+import Competition.RobotMap;
+import Competition.Subsystems.DriveSubsystem;
+import Competition.Subsystems.HookSubsystem;
+import DubinsCurve.curveProcessor3;
+import FtcExplosivesPackage.ExplosiveAuto;
+import Utilities.Utility;
+
+@Autonomous (name = "New Blue Foundation")
+public class NewBlueFoundation extends ExplosiveAuto {
+
+    DriveSubsystem drive;
+    HookSubsystem hooker;
+    curveProcessor3 curve;
+    Utility u = new Utility(this);
+
+    @Override
+    public void initHardware() {
+        RobotMap robotMap = new RobotMap(hardwareMap);
+        Robot robot = new Robot(this);
+        robot.enable();
+
+        Robot.track.setCurrentNode(1, -3, 90);
+        RobotMap.gyro.startAng = 90;
+
+        drive = Robot.drive;
+        hooker = Robot.hooker;
+
+        curve = new curveProcessor3(drive, telemetry, this);
+    }
+
+    @Override
+    public void initAction() {
+
+    }
+
+    @Override
+    public void body() throws InterruptedException {
+        drive.moveStrafePow(-1, 500);
+        drive.moveTurnPID(90);
+        drive.moveStrafePow(-0.5, 300);
+        drive.moveRangePID(14, 5000, false);
+        hooker.hook();
+        u.waitMS(1000);
+        drive.moveStrafePow(0.7, 1000);
+        drive.moveTurnFound(5);
+        drive.moveStrafePow(-0.7, 1000);
+        hooker.release();
+        drive.moveStrafePow(1, 1000);
+    }
+
+    @Override
+    public void exit() throws InterruptedException {
+
+    }
+}
